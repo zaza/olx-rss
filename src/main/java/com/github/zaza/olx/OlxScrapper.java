ed7138c0c0dec7265e3c8dbfadb0e2ed9995c5ce
Tomasz Zarna <tzarna@gmail.com>
2017-09-02 00:48:32 +0200

    remove unused import

--- src/main/java/com/github/zaza/olx/OlxScrapper.java
+++ src/main/java/com/github/zaza/olx/OlxScrapper.java
@@ -9,7 +9,6 @@ import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
