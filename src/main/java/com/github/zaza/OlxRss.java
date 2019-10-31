a2e4316cfe65dea53f5a460436cbda07d0c17ec5
Tomasz Zarna <tzarna@gmail.com>
2019-10-31 23:04:05 +0100

    deny requests without an auth token

--- src/main/java/com/github/zaza/OlxRss.java
+++ src/main/java/com/github/zaza/OlxRss.java
@@ -2,8 +2,16 @@ package com.github.zaza;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.before;
+import static spark.Spark.halt;
 
 import com.github.zaza.olx.OlxClient;
+import com.google.common.base.Strings;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import spark.FilterImpl;
+import spark.Request;
+import spark.Response;
 
 public class OlxRss {
 
@@ -11,9 +19,35 @@ public class OlxRss {
 	}
 
 	public static void main(String[] args) {
-		port(Integer.valueOf(System.getenv("PORT")));
+		Dotenv dotenv = Dotenv.load();
+		port(Integer.valueOf(dotenv.get("PORT")));
+		before(new TokenAuthenticationFilter(dotenv.get("TOKEN"))); 
 		get("/rss", (req, res) -> new FeedWriter()
 				.write(new OlxClient(req).search()));
 	}
+	
+	private static class TokenAuthenticationFilter extends FilterImpl {
+
+		private final String validToken;
+
+		protected TokenAuthenticationFilter(String token) {
+			super("/*", "*/*");
+			this.validToken = token;
+		}
+
+		@Override
+		public void handle(Request req, Response res) throws Exception {
+			if (Strings.isNullOrEmpty(validToken)) {
+				halt(500, "token not configured");
+			}
+			String token = req.queryParams("token");
+			if (token == null) {
+				halt(401, "missing token");
+			}
+			if (!validToken.equals(token)) {
+				halt(401, "invalid token");
+			}
+		}
+	} 
 
 }
