05ff0b7cb6787e710dd1310158f625a295e49c20
zaza <tzarna@gmail.com>
2021-08-02 00:05:10 +0200

    update guava to latest and fix related errors in tests

--- src/test/java/com/github/zaza/olx/OlxQueryBuilderTest.java
+++ src/test/java/com/github/zaza/olx/OlxQueryBuilderTest.java
@@ -1,23 +1,19 @@
 package com.github.zaza.olx;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class OlxQueryBuilderTest {
-	
-	@Rule
-    public ExpectedException thrown= ExpectedException.none();
-	
+
 	@Test
 	public void getDescription() throws Exception {
 		OlxQueryBuilder builder = OlxQueryBuilder.query("foo");
 
 		assertThat(builder.getDescription()).isEqualTo("'foo'");
 	}
-	
+
 	@Test
 	public void getDescriptionWithPhotoOnly() throws Exception {
 		OlxQueryBuilder builder = OlxQueryBuilder.query("foo").withPhotoOnly();
@@ -27,43 +23,38 @@ public class OlxQueryBuilderTest {
 
 	@Test
 	public void whenSpecifiyingRadiusLocationMustBeProvidedFirst() {
-		thrown.expect(IllegalStateException.class);
-		thrown.expectMessage("Location must be provided first.");
-		OlxQueryBuilder.query("foo").radius(Radius._0KM);
+		assertThatThrownBy(() -> OlxQueryBuilder.query("foo").radius(Radius._0KM))
+				.isInstanceOf(IllegalStateException.class).hasMessage("Location must be provided first.");
 	}
 
 	@Test
 	public void minPriceMustBeZeroAtLeast() {
-		thrown.expect(IllegalArgumentException.class);
-		thrown.expectMessage("Min price must be 0 at least.");
-		OlxQueryBuilder.query("foo").minPrice(-1);
+		assertThatThrownBy(() -> OlxQueryBuilder.query("foo").minPrice(-1)).isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("Min price must be 0 at least.");
 	}
 
 	@Test
 	public void maxPriceMustBeZeroAtLeast() {
-		thrown.expect(IllegalArgumentException.class);
-		thrown.expectMessage("Max price must be 0 at least.");
-		OlxQueryBuilder.query("foo").maxPrice(-1);
+		assertThatThrownBy(() -> OlxQueryBuilder.query("foo").maxPrice(-1)).isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("Max price must be 0 at least.");
 	}
 
 	@Test
 	public void minPriceMustLessThenMaxPrice() {
-		thrown.expect(IllegalArgumentException.class);
-		thrown.expectMessage("Min price must be less than max price.");
-		OlxQueryBuilder.query("foo").maxPrice(1).minPrice(2);
+		assertThatThrownBy(() -> OlxQueryBuilder.query("foo").maxPrice(1).minPrice(2))
+				.isInstanceOf(IllegalArgumentException.class).hasMessage("Min price must be less than max price.");
 	}
 
 	@Test
 	public void maxPriceMustMoreThenMinPrice() {
-		thrown.expect(IllegalArgumentException.class);
-		thrown.expectMessage("Max price must be more than max price.");
-		OlxQueryBuilder.query("foo").minPrice(2).maxPrice(1);
+		assertThatThrownBy(() -> OlxQueryBuilder.query("foo").minPrice(2).maxPrice(1))
+				.isInstanceOf(IllegalArgumentException.class).hasMessage("Max price must be more than max price.");
 	}
 
 	@Test
 	public void minPriceDefinedTwice() {
 		OlxQueryBuilder builder = OlxQueryBuilder.query("foo").minPrice(2).minPrice(1);
-		
+
 		assertThat(builder.getPrice().lowerEndpoint()).isEqualTo(1);
 		assertThat(builder.getPrice().hasUpperBound()).isFalse();
 	}
@@ -71,7 +62,7 @@ public class OlxQueryBuilderTest {
 	@Test
 	public void maxPriceDefinedTwice() {
 		OlxQueryBuilder builder = OlxQueryBuilder.query("foo").maxPrice(2).maxPrice(1);
-		
+
 		assertThat(builder.getPrice().hasLowerBound()).isFalse();
 		assertThat(builder.getPrice().upperEndpoint()).isEqualTo(1);
 	}
@@ -79,7 +70,7 @@ public class OlxQueryBuilderTest {
 	@Test
 	public void minAndMaxPriceDefined() {
 		OlxQueryBuilder builder = OlxQueryBuilder.query("foo").minPrice(1).maxPrice(2);
-		
+
 		assertThat(builder.getPrice().lowerEndpoint()).isEqualTo(1);
 		assertThat(builder.getPrice().upperEndpoint()).isEqualTo(2);
 	}
@@ -87,7 +78,7 @@ public class OlxQueryBuilderTest {
 	@Test
 	public void maxAndMinPriceDefined() {
 		OlxQueryBuilder builder = OlxQueryBuilder.query("foo").maxPrice(2).minPrice(1);
-		
+
 		assertThat(builder.getPrice().lowerEndpoint()).isEqualTo(1);
 		assertThat(builder.getPrice().upperEndpoint()).isEqualTo(2);
 	}
